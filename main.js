964efaecb60a1538ac8f5f7f9e80b0efcf8ee098
Chetan Barot <chetanbarot@hotmail.com>
2016-06-12 08:45:57 +0100

    created game logic

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ var cardOne = "queen";
 var cardTwo ="queen";
 var cardThree ="king";
 var cardFour ="king";
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+}
+if (cardThree === cardFour){
+	alert('you found a match!');
+}
+else {
+	alert('Sorry, try again.');
+}
